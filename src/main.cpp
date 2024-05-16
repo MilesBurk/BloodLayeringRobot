80e34fd44d841483eba0338496ab410853942943
Kevin Choi <kcwc120@gmail.com>
2024-05-16 11:39:19 -0700

    New dispense

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@
 #define pumpPin 14
 #define pumpMicrosteps 16
 
-#define pumpDirectionPin 23 //NOTE THAT FORWARD DIRECTION FOR THE PUMP IS TRUE/HIGH
+#define pumpDirectionPin 5 //NOTE THAT FORWARD DIRECTION FOR THE PUMP IS TRUE/HIGH
 
 #define volumeSensingPin 23
 
@@ -59,7 +59,7 @@ gantry Gantry = gantry();
 int startingZ_mm  = Gantry.getMaxZDisplacement() - 50;
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-
+//test
 int servo1Pos = 0;
 
 int servoPos_pulse[4] = {0, 0, 0, 0};
@@ -149,7 +149,6 @@ void loop() {
   
   
   performFillingMotionFor1Tube();
-
 
   /*
     Gantry.goToAbsPosition_mm(Gantry.getXDisplacement_um()/1000, startingY_mm, startingZ_mm, 5);
@@ -523,15 +522,15 @@ void performFillingMotionFor1Tube(){
 
   //find distance to move out of the tube
     //this is the diagonal distance out of the tube you with to travel, I assume it is just 1cm shy of where you started so as to ensure you are in the tube at the end
-    int exitDistance_um = entranceDistance_um - 10000;
+    int exitDistance_um = entranceDistance_um - 30000;
   //the line below pulls the tube out in 1 shot where as the loop lets you set different pump speeds as you pull it out.
   //Gantry.goToRelativePosition(0, exitDistance_um*sin(PI*firstFillAngle/float(180)), exitDistance_um*cos(PI*firstFillAngle/float(180)), 5000);
    
    
       //HERE I LET YOU DO DIFFERENT PUMP SEQUENCES AS YOU FILL IT UP
-   int pumpRPMS[] = {10, 40, 60, 80};
+   int pumpRPMS[] = {5, 7, 9, 11, 20, 0};
    //MAKE SURE BOTH THESE ARRAYS HAVE SAME NUMBER OF ELEMENTS!!
-   int delays_ms_Per_pumpingInterval[] = {3000, 5000, 3000, 2000};
+   int delays_ms_Per_pumpingInterval[] = {3000, 5000, 3000, 2000, 1000, 1000};
 
 
    int numberOfPumpingSequencesWhileExitingTube = sizeof(pumpRPMS)/sizeof(int);
@@ -541,30 +540,7 @@ void performFillingMotionFor1Tube(){
     setPumpRPM(pumpRPMS[i], pumpPin, pumpMicrosteps);
     Gantry.goToRelativePosition(0, exitDistancePerPumpSequence_um*sin(PI*firstFillAngle/float(180)), exitDistancePerPumpSequence_um*cos(PI*firstFillAngle/float(180)), delays_ms_Per_pumpingInterval[i]);
    }
-   
-
-    //BELOW I LEFT THE OLD PUMPING SEQUENCE FOR YOUR REFERENCE IF IT IS STILL USEFUL for reference of speeds and delay times
-   /*
-    //ADD PUMPING SEQUENCE HERE
-  setPumpRPM(10, pumpPin, pumpMicrosteps);
-  //delay(3*60000);
-  setPumpRPM(0, pumpPin, pumpMicrosteps);
-
-  entranceDistance_um = 15000;
-  Gantry.goToRelativePosition(0, entranceDistance_um*sin(PI*firstFillAngle/float(180)), entranceDistance_um*cos(PI*firstFillAngle/float(180)), 5000);
-
-       delay(10000);
-  setPumpRPM(5, pumpPin, pumpMicrosteps);
-  delay(60000);
-  setPumpRPM(7, pumpPin, pumpMicrosteps);
-  delay(30000);
-  setPumpRPM(10, pumpPin, pumpMicrosteps);
-  delay(30000);
-  setPumpRPM(15, pumpPin, pumpMicrosteps);
-  delay(30000);
-  setPumpRPM(0, pumpPin, pumpMicrosteps);
-   */
-  
+     
   //take the nozzle out by traveling straight up.
   Gantry.goToRelativePosition(0, 0, 40000, 5000);
 
@@ -603,6 +579,13 @@ void performFillingMotionFor1Tube(){
 
   //go back to center
   servo1Pos = sweepToAngle(0, 2, tube1, tube1_Offset, servo1Pos);
+
+  delay(1000);
+  setPumpDirection(false);
+  setPumpRPM(300, pumpPin, pumpMicrosteps);
+  delay(3000);
+  setPumpRPM(0, pumpPin, pumpMicrosteps);
+  setPumpDirection(true);
 
 }
 
